00e239c77ce873c45cf7e88560f5578948cba80e
hetjagani <hetpatel572@gmail.com>
2021-02-12 12:11:19 -0800

    Added basic code to test fire webhooks.

--- util/subscribe.go
+++ util/subscribe.go
@@ -3,21 +3,20 @@ package util
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"strings"
+	"time"
 
 	"github.com/factly/web-hooks-service/config"
 	"github.com/factly/web-hooks-service/model"
+	"github.com/factly/x/requestx"
 	"github.com/nats-io/nats.go"
 )
 
 // SubscribeEvents subscribe one or more events
 func SubscribeEvents(events ...string) {
 	for _, event := range events {
-		NC.Subscribe(event, func(m *nats.Msg) {
-			var payload map[string]interface{}
-			json.Unmarshal(m.Data, &payload)
-
-			fmt.Printf("Received a [%v] event with data: %v\n", m.Subject, payload)
-		})
+		NC.Subscribe(event, FireWebhooks)
 	}
 }
 
@@ -35,4 +34,36 @@ func SubscribeExistingEvents() {
 	}
 
 	SubscribeEvents(eventNames...)
+}
+
+// FireWebhooks fires webhook for a event
+func FireWebhooks(m *nats.Msg) {
+	var payload map[string]interface{}
+	json.Unmarshal(m.Data, &payload)
+
+	whData := model.WebhookData{}
+
+	whData.Event = m.Subject
+	whData.CreatedAt = time.Now()
+	whData.Contains = []string{strings.Split(m.Subject, ".")[0]}
+	whData.Payload = payload
+
+	fmt.Printf("Received a [%v] event with data: %v\n", m.Subject, payload)
+
+	// TODO: find all the registered webhooks for given event and fire on each url through go routines
+
+	url := "https://cffaf321acf0589eee315b01d3087c70.m.pipedream.net" // test url
+
+	// Create a log entry in WebhookLog
+
+	resp, err := requestx.Request("POST", url, whData, nil)
+	if err != nil {
+		fmt.Println("webhook at ", url, "failed...")
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("webhook at ", url, "failed...")
+		return
+	}
 }
